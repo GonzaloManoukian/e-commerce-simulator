3b51a5c2b2b86426db5cb3df1631beabb4aa359d
Gonzalo Manoukian <gonzalomanoukian@gmail.com>
2020-12-08 16:47:07 -0300

    Desafio 4

--- funciones.js
+++ funciones.js
@@ -1,17 +1,14 @@
-let usuario = prompt("Ingrese su nombre y apellido");
-let verificacion = prompt("Vuelva a ingresar su nombre y apellido para verificar que no es un robot");
+let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
-if(usuario == verificacion) {
-    console.log("Usuario: " + usuario);
-    alert("Bienvenido " + usuario);
-
-    let numero = parseInt(prompt("Ingrese un Numero"))
-    
-    if(numero >= 10 && numero <= 50) {
-        alert("Su numero se encuentra entre 10 y 50")
-    } else {
-        alert("Su numero es menor a 10 o mayor a 50")
-    }
+for(let i=0; i<7; i++) {
+    if(i==6){
+        alert(`Hoy es ${dias[i]}!!! Llegaste al fin de la semana!`);
+        console.log(`Hoy es ${dias[i]}!!! Llegaste al fin de la semana!`);
+    } else if (i%2==0){
+    alert(`El ${dias[i]} es dia par`)       
+    console.log(`El ${dias[i]} es dia par`);
 } else {
-    alert("Usuario Incorrecto, Intente Nuevamente.")
+    alert(dias[i]);
+    console.log(dias[i]);
+}
 }
